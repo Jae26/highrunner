18d58c783430cea6ac6efa40b928217e9665e5c3
Antti <antti.vornanen@gmail.com>
2018-05-07 21:11:37 +0300

    Added enemy movement

---
+++ 2D Projekti - tests_coding/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+    public float speed;
+    private bool movingRight = true;
+    public Transform groundDetection;
+    public Transform buildingDetection;
+    public float distance;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D grounInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if(grounInfo.collider.CompareTag("building") == false)
+        {
+            if (movingRight == true)
+            {
+                Debug.Log("Hitted air1");
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                Debug.Log("Hitted air2");
+            }
+        }
+        RaycastHit2D builingInfo = Physics2D.Raycast(buildingDetection.position, Vector2.right, distance);
+        if (builingInfo.collider.CompareTag("building") == true)
+        {
+            if (movingRight == true)
+            {
+                Debug.Log("Hitted air1");
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                Debug.Log("Hitted air2");
+            }
+        }
+    }
+}
